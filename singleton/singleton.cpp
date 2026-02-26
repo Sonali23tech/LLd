5941b904c7326df7a409a50dd1c7387f46e19b2e
Singh, Sonali <sonali.singh@united.com>
2026-02-26 20:21:10 +0530

    Singleton_desgin pattern

--- singleton/singleton.cpp
+++ singleton/singleton.cpp
@@ -1,34 +1,36 @@
 #include<iostream>
-#include "singleton.hpp"
 using namespace std;
 
+class Singleton {
+private:    
+    static Singleton* instance;  // Static pointer to the single instance
 
-Logger* Logger::loggerInstance = nullptr;
-int Logger::ctr = 0;
-mutex Logger::mtx;
-Logger::Logger()
-{
-    ctr++;
-    cout<<"New instance created.No of instances "<< ctr << endl;
-}
-
-void Logger::log(string message)
-{
-    cout<<"Log: "<<message<<endl;
-}
-
-
-Logger* Logger::getLogger()
-{
-    if(loggerInstance == nullptr){
-    mtx.lock();
-    if(loggerInstance == nullptr)
-    {
-        loggerInstance = new Logger();
+    // Private constructor to prevent instantiation
+    Singleton() {
+        cout << "Singleton instance created." << endl;
     }
 
-    mtx.unlock();
-}
+public:
+    // Static method to provide access to the instance  
+    static Singleton* getInstance() {
+        if (instance == nullptr) {  // Check if instance already exists
+            instance = new Singleton();  // Create the instance if it doesn't exist
+        }
+        return instance;  // Return the single instance
+     }
+};
 
-    return loggerInstance;
+// Initialize the static member variable
+Singleton* Singleton::instance = nullptr;       
+int main() {
+    Singleton* s1 = Singleton::getInstance();  // Creates the instance
+    Singleton* s2 = Singleton::getInstance();  // Returns the existing instance
+
+    if (s1 == s2) {
+        cout << "Both pointers point to the same instance." << endl;
+    } else {
+        cout << "Different instances exist." << endl;
+    }
+
+    return 0;
 }
